67d0b13b7dec082d5f708c63655e3687241af719
jason suen <jasonben579@gmail.com>
2020-03-01 20:47:10 +0800

    chatRoom Complete

---
+++ Assets/Script/ChatClient.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Linq;
+using System.Net;
+using UnityEngine;
+
+public class ChatClient
+{
+    public ChatRoomManager RoomManager;
+    TcpClient mClient = null;
+    string mAddress = "";
+    int mPort;
+
+    public ChatClient()
+    { 
+    
+    }
+
+
+    public bool Connect(string _address, int _port)
+    {
+        mAddress = _address;
+        mPort = _port;
+
+        mClient = new TcpClient();
+
+        try
+        {
+            IPHostEntry host = Dns.GetHostEntry(_address);
+            IPAddress address = null;
+            foreach (IPAddress h in host.AddressList)
+            {
+                if (h.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    address = h;
+                    break;
+                }
+            }
+            mClient.Connect(address.ToString(), _port);
+            Debug.Log("Connected to Chat Server: " + _address + ":" + _port + "\n");
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Exception happened: " + e.ToString());
+            return false;
+        }
+    }
+
+    public void SendName(string _name)
+    {
+        string request = "LOGINNAME:" + _name;
+        byte[] requestBuffer = System.Text.Encoding.ASCII.GetBytes(request);
+        mClient.GetStream().Write(requestBuffer, 0, requestBuffer.Length);
+    }
+
+    public void SendBroadcast(string _message)
+    {
+        string request = "BROADCAST:" + _message;
+        byte[] requestBuffer = System.Text.Encoding.ASCII.GetBytes(request);
+        mClient.GetStream().Write(requestBuffer, 0, requestBuffer.Length);
+    }
+
+    public void Run()
+    {
+        if (mClient.Available > 0)
+        {
+            HandleReceiveMessages(mClient);
+        }
+    }
+
+    public void HandleReceiveMessages(TcpClient tcpClient)
+    {
+        NetworkStream tcpClientStream = tcpClient.GetStream();
+
+        int numByte = tcpClient.Available;
+        byte[] buffer = new byte[numByte];
+        int byteRead = tcpClientStream.Read(buffer, 0, buffer.Length);
+
+        string request = System.Text.Encoding.ASCII.GetString(buffer).Substring(0, byteRead);
+
+        if (request.StartsWith("MESSAGE:", StringComparison.OrdinalIgnoreCase))
+        {
+            string[] aTokens = request.Split(':');
+            string sName = aTokens[1];
+            string sMessage = aTokens[2];
+            //Debug.Log(sName + " said: " + sMessage);
+            RoomManager.MessageShow(sName + " said: " + sMessage);
+        }
+    }
+}
---
+++ Assets/Script/ChatRoomManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ChatRoomManager : MonoBehaviour
+{
+
+    public int status = 0;
+
+    public InputField IP;
+    public InputField Name;
+    public InputField Speak;
+
+    public ScrollRect scroll;
+
+    public Text messageSimple;
+
+    private GameObject page_IP;
+    private GameObject page_Name;
+    private GameObject page_Speak;
+
+    ChatClient client = null;
+
+    bool connectSucceed = false;
+    float timer = 0.0f;
+
+    private void Awake()
+    {
+        page_IP = transform.GetChild(1).gameObject;
+        page_Name = transform.GetChild(2).gameObject;
+        page_Speak = transform.GetChild(3).gameObject;
+
+    }
+
+    private void Update()
+    {
+        if (connectSucceed == false)
+        {
+            return;
+        }
+
+        RunStatus();
+    }
+
+    /// <summary>
+    /// 跑狀態
+    /// </summary>
+    private void RunStatus()
+    {
+        if (status == 0)
+        {
+            if (timer <= 0.01)
+            {
+                client.Run();
+                Debug.Log("run");
+                timer = 1.5f;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+
+        }
+        else if (status == 1)
+        {
+            client.SendBroadcast(Speak.text);
+            Speak.text = "";
+            status = 0;
+        }
+    }
+
+    /// <summary>
+    /// 輸入ip
+    /// </summary>
+    public void SendIP()
+    {
+        page_IP.SetActive(false);
+        page_Name.SetActive(true);
+    }
+
+    /// <summary>
+    /// 輸入名稱，連線
+    /// </summary>
+    public void SendName()
+    {
+        client = new ChatClient();
+        client.RoomManager = this;
+        connectSucceed = client.Connect(IP.text, 4099);
+        if (connectSucceed)
+        {
+            page_Name.SetActive(false);
+            page_Speak.SetActive(true);
+            client.SendName(Name.text);
+        }
+    }
+
+    /// <summary>
+    /// 傳送訊息
+    /// </summary>
+    public void SendMessage()
+    {
+        status = 1;
+    }
+
+    /// <summary>
+    /// 顯示訊息出來
+    /// </summary>
+    public void MessageShow(string _message)
+    {
+        RectTransform parentObj = scroll.content;
+        parentObj.sizeDelta += new Vector2(0, 60);
+        int counts = parentObj.childCount;
+        Text copt = Instantiate(messageSimple, parentObj);
+        copt.text = _message;
+        copt.transform.localScale = Vector3.one;
+        copt.transform.localPosition = new Vector3(0, counts * 60, 0);
+        copt.gameObject.SetActive(true);
+    }
+}
